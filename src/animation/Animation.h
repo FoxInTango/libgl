504363be5716eff44f25af192f50af74bb16098a
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-19 20:21:46 +0800

    Fri Sep 19 08:21:46 PM CST 2025

--- src/animation/Animation.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LIBGL_ANIMATION_H_
-#define _LIBGL_ANIMATION_H_
-#include <libcpp/libcpp.h>
-EXTERN_C_BEGIN
-namespaceBegin(foxintango)
-class foxintangoAPI Animation{};
-namespaceEnd
-#endif
---
+++ src/model/Model.h
@@ -0,0 +1,13 @@
+#ifndef _LIBGL_MODEL_H_
+#define _LIBGL_MODEL_H_
+#include <libcpp/libcpp.h>
+EXTERN_C_BEGIN
+namespaceBegin(foxintango)
+namespaceBegin(graphics)
+class foxintangoAPI Model{
+
+};
+namespaceEnd
+namespaceEnd
+EXTERN_C_END
+#endif
